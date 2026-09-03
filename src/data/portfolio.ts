38a91ce9598b865d4370fe283d7a51264e4feae7
OVO\monic <monicaliang2007@gmail.com>
2026-09-02 20:09:22 -0600

    small update

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -165,7 +165,7 @@ export const portfolio = {
   ],
 
   contact: {
-    phone: '(587) 917-9870',
+    phone: '',
     location: 'Calgary, AB',
     email: 'monica.leung.w@gmail.com',
     github: 'https://github.com/MonicaLovo',
